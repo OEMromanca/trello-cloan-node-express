f0f1f0119dc88d9e4a3e0a4b9d43c04212f728bb
Tvoje meno <tvoj-email@example.com>
2024-10-08 15:14:16 +0200

    minor fixes

--- .history/src/server_20241008151351.js
+++ .history/src/server_20241008151351.js
@@ -34,6 +34,6 @@ const server = https.createServer(
   app
 );
 
-server.listen(port, '0.0.0.0',() => {
+server.listen('0.0.0.0:5000',() => {
   console.log(`Server is running at https://localhost:${port}`);
 });
