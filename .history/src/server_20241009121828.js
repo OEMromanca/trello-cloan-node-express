a94c0728dd9a92b91fe8488d1462be435347c1c6
Tvoje meno <tvoj-email@example.com>
2024-10-09 12:18:57 +0200

    minor changes

--- .history/src/server_20241009121828.js
+++ .history/src/server_20241009121828.js
@@ -11,7 +11,7 @@ require('./db');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000; // Uisti sa, že používaš PORT z prostredia Renderu
 
 app.use(cors());
 app.use(cookieParser());
