87323b22a74ca7d6133a9c85031adfc5af2a9546
Tvoje meno <tvoj-email@example.com>
2024-10-10 13:41:01 +0200

    csrf minor fixes

--- src/controllers/csrfController.js
+++ src/controllers/csrfController.js
@@ -1,12 +1,6 @@
 const getCsrfToken = (req, res) => {
   const csrfToken = req.csrfToken();
-
-  res.cookie('XSRF-TOKEN', csrfToken, {
-    httpOnly: false,  
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'None',  
-    maxAge: 3600000 
-  });
+ 
 
   res.json({ csrfToken });
 };
