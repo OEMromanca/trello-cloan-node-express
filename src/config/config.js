9a784492f8d7b16271bf5c17127239f6361c9764
Tvoje meno <tvoj-email@example.com>
2024-10-10 14:32:56 +0200

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const csurf = require('csurf');
 
 const secretKey = crypto.randomBytes(32).toString('hex');
-const csrfProtection = csurf({ cookie: false });
+const csrfProtection = csurf({ cookie: true });
 
 module.exports = {
   secretKey,
