9f1b3bb9caa03ec9934a65f657f80b0e0da16932
Tvoje meno <tvoj-email@example.com>
2024-10-10 14:23:48 +0200

    csrf

--- src/controllers/csrfController.js
+++ src/controllers/csrfController.js
@@ -1,6 +1,12 @@
 const getCsrfToken = (req, res) => {
   const csrfToken = req.csrfToken();
- 
+  res.cookie('XSRF-TOKEN', csrfToken, {
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+  });
+  res.locals.csrftoken = req.csrfToken();
+
 
   res.json({ csrfToken });
 };
