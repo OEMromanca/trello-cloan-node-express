771c36a6573ea23c72b783807802c1577e583c46
Tvoje meno <tvoj-email@example.com>
2024-10-14 14:49:14 +0200

    minor csrf fixes

--- src/controllers/csrfController.js
+++ src/controllers/csrfController.js
@@ -1,11 +1,6 @@
 const getCsrfToken = (req, res) => {
   const csrfToken = req.locals.csrfToken();
 
-  res.cookie('XSRF-TOKEN', csrfToken, {
-    httpOnly: false, 
-    sameSite: 'None', 
-    secure:true,
-  });
   res.json({ csrfToken });
 };
 
