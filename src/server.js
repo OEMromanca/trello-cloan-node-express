b8075cd172f2cfe06f595e88604a7aae1c93f178
Tvoje meno <tvoj-email@example.com>
2024-10-14 14:43:51 +0200

    cors minor changes

--- src/server.js
+++ src/server.js
@@ -15,8 +15,11 @@ const app = express();
 const port = process.env.PORT || 5000;  
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
-  credentials: true, 
+  origin: [
+    'http://localhost:5173', 
+    'https://trello-clone-0ln5.onrender.com'  
+  ],  
+  credentials: true,  
 }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
