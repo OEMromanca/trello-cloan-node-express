7868ca9aa21666fdb825b4840d56b369a21145f8
Tvoje meno <tvoj-email@example.com>
2024-10-16 13:54:44 +0200

    cors fixes

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'https://trello-clone-0ln5.onrender.com',
-
+    'https://trello-clone-react.onrender.com'
   ],  
   allowedHeaders: ['Content-Type', 'X-CSRF-Token'],
 
