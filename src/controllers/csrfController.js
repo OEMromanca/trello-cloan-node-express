2e2bcf08eabe3a30daa535b9348c36fffba63673
Tvoje meno <tvoj-email@example.com>
2024-10-16 14:54:38 +0200

    minor csrf fixes

--- src/controllers/csrfController.js
+++ src/controllers/csrfController.js
@@ -1,15 +1,15 @@
-const getCsrfToken = (req, res, next) => {
+const getCsrfToken = (req, res) => {
   const csrfToken = req.csrfToken();
 
-  console.log("token")
+  console.log("token");
 
   res.cookie('XSRF-TOKEN', csrfToken, {
     httpOnly: false,
     sameSite: 'None', 
-    secure:true,
+    secure: true,
   });
-  next();
-};
 
+  res.json({ 'XSRF-TOKEN': csrfToken }); 
+};
 
 module.exports = { getCsrfToken };
